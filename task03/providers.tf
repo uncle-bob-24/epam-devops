ba28fd761f1f98286a7f6a396ded6b90e1a4cf66
AK <ak@ak.com>
2025-08-28 22:05:01 +0500

    task03

---
+++ task03/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "e887a2a5-320e-4655-a4b3-6271b168e9df"
+}
---
+++ task03/terraform.tfvars
@@ -0,0 +1,19 @@
+# String variable
+resource_group_location         = "eastus"
+environment                     = "production"
+
+# Number variable
+
+
+# Boolean variable
+
+
+# List variable
+
+
+# Map variable
+default_tag = {
+  Creator = "azamat_kireyev@epam.com"
+}
+
+# Object variable
