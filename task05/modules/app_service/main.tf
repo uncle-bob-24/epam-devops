effb5f410a5c7e724671063c73abcdb5c7e66fcb
AK <azamatsm@gmail.com>
2025-09-06 19:19:24 +0500

    task05 add ip_restriction_default_action = Deny

--- task05/modules/app_service/main.tf
+++ task05/modules/app_service/main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_windows_web_app" "app" {
   for_each = var.web_apps
 
-  name                          = each.value.name
-  location                      = each.value.location
-  resource_group_name           = each.value.resource_group_name
-  service_plan_id               = each.value.app_service_plan_id
-  public_network_access_enabled = false
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+  service_plan_id     = each.value.app_service_plan_id
 
   site_config {
-    always_on = true
+    always_on                     = true
+    ip_restriction_default_action = "Deny"
 
     dynamic "ip_restriction" {
       for_each = each.value.ip_restrictions
