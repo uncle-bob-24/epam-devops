dffce0710a7c41aef78167bc19131a29aaabe736
AK <azamatsm@gmail.com>
2025-09-10 23:15:55 +0500

    task06 fix check 29

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,5 +1,5 @@
 output "sql_connection_string" {
-  value = format("Server=tcp:%s.database.windows.net,1433;Database=%s;User ID=%s;Password=%s;Trusted_Connection=False;Encrypt=True;",
+  value = format("Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
     azurerm_mssql_server.mssql_server.fully_qualified_domain_name,
     azurerm_mssql_database.sql_database.name,
     var.sql_admin_name,
