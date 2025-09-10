7f6fbede04ced034ca24b220737d2173a5d6a52a
AK <azamatsm@gmail.com>
2025-09-11 01:11:38 +0500

    task06 fix check 13

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,12 +1,12 @@
 output "sql_connection_string" {
+  sensitive = true
   value = format(
-    "Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+    "Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30",
     azurerm_mssql_server.mssql_server.fully_qualified_domain_name, # {server_name}
     azurerm_mssql_database.sql_database.name,                      # {db_name}
     var.sql_admin_name,                                            # {sql_user_name}
     random_password.sql_admin_password.result                      # {sql_user_password}
   )
-  sensitive = true
 }
 
 output "sql_server_fqdn" {
