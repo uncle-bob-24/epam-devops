407bff4b50c8d9eb37e1cf92f196214293e4535b
AK <azamatsm@gmail.com>
2025-09-22 16:06:30 +0500

    task08 fix check15

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_container_registry_task" "acr_task" {
 
   docker_step {
     dockerfile_path      = var.dockerfile_path
-    context_path         = "${var.repository_url}#main:${trimsuffix(var.dockerfile_path, "/Dockerfile")}"
+    context_path         = "."
     context_access_token = var.git_pat
     image_names          = ["${var.docker_image_name}:latest"]
   }
